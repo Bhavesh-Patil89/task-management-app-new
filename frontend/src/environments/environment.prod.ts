4e357056f16aa4faf6b24bcc2a27210b58909f18
Bhavesh-Patil89 <patilbhaveshk89@gmail.com>
2025-02-01 16:08:24 +0530

    Describe your changes here

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.prod.ts
-import { Environment } from './environment';
-
-export const environment: Environment = {
-  production: true,
-  apiUrl: 'https://task-management-app-cgba.onrender.com/api'
-};
